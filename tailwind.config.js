c6e14a08534e33321c42fe7643a1f8cfdb2220a1
su-re-sh-75 <suresh22072003@gmail.com>
2024-03-07 15:56:36 +0530

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./Baby_app/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'baby-red':{
+          900:"#BE3857",
+          800:"#C89099",
+          700:"#E3BFC4"
+        }
+      }
+    },
   },
   plugins: [],
 }
