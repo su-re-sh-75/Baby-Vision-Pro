0ae7cc93c6064ab172158e673f38bca824f14ff0
su-re-sh-75 <suresh22072003@gmail.com>
2024-04-21 22:57:02 +0530

    shown db data

--- Baby_app/views.py
+++ Baby_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 def index(request):
     return render(request, 'Baby_app/index.html')
@@ -17,7 +19,31 @@ def register(request):
     
 
 def dashboard(request):
-    if request.method == 'POST':
-        temp = request.POST['temp']
-        msg = request.POST['msg']
-    return render(request, 'Baby_app/dashboard.html')
+    
+    bucket = "sensors"
+    org = "BVP"
+    token = "FOxPmFk6mKxzuqlWUC5AKwWZzXl1L8LIQ3wFhqL2l3DAMrSxPkAoB11vkfpijHGzhtDCZY4tsd3pvGxOjQFGMA=="
+    url="http://localhost:8086"
+
+    client = influxdb_client.InfluxDBClient(
+        url=url,
+        token=token,
+        org=org
+    )
+
+    # Query script
+    query_api = client.query_api()
+
+    query = 'from(bucket:"sensors")\
+    |> range(start: -30d)\
+    |> filter(fn:(r) => r._measurement == "sensors")\
+    |> filter(fn:(r) => r.location == "Prague")\
+    |> filter(fn:(r) => r._field == "temperature")'
+
+    result = query_api.query(org=org, query=query)
+    context = {'temperature':[]}
+    for table in result:
+        for record in table.records:
+            context['temperature'].append((record.get_time(), record.get_value()))
+
+    return render(request, 'Baby_app/dashboard.html', context=context)
